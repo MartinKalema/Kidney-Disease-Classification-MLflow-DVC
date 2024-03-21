a310d668f9900ca309d1c5ba09e1c219805e5985
MartinKalema <kalemaaaaaaaa@gmail.com>
2024-03-21 23:25:08 +0300

    Created Logger

---
+++ src/kidneyDiseaseClassifier/__init__.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import logging
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+log_dir = "logs" # Directory where the logs are stored
+log_file_path = os.path.join(log_dir, "running_logs.log") # Path to log file
+os.makedirs(log_dir, exist_ok=True)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=logging_str, # Specifies the format of the log messages
+    handlers=[
+        logging.FileHandler(log_file_path), # Log messages to a file at a specific path
+        logging.StreamHandler(sys.stdout) # Log messages to the console
+    ]
+)
+
+logger = logging.getLogger('kidneyDiseaseClassifierLogger') # Creates a logger object
