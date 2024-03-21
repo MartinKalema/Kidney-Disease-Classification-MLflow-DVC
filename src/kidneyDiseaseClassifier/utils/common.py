ecea9189e15776514fb500de8900be15c5fb0cdf
MartinKalema <kalemaaaaaaaa@gmail.com>
2024-03-22 00:25:07 +0300

    read yaml file

--- src/kidneyDiseaseClassifier/utils/common.py
+++ src/kidneyDiseaseClassifier/utils/common.py
@@ -9,3 +9,25 @@ from box import ConfigBox
 from pathlib import Path
 from typing import Any
 import base64
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns
+
+    Args: 
+        path_to_yaml (str): path like input
+
+    Raises: 
+        ValueError: if yaml file is empty
+        e: empty file
+    Returns:
+        ConfigBox: ConfigBox type
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
