7b79c1ad6464d70d82db534906abe67e414aa7ab
MartinKalema <kalemaaaaaaaa@gmail.com>
2024-03-24 21:18:34 +0300

    linting

--- src/kidneyDiseaseClassifier/__init__.py
+++ src/kidneyDiseaseClassifier/__init__.py
@@ -4,17 +4,17 @@ import logging
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 
-log_dir = "logs" # Directory where the logs are stored
-log_file_path = os.path.join(log_dir, "logs.log") # Path to log file
+log_dir = "logs" 
+log_file_path = os.path.join(log_dir, "logs.log") 
 os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(
     level=logging.INFO,
-    format=logging_str, # Specifies the format of the log messages
+    format=logging_str, 
     handlers=[
-        logging.FileHandler(log_file_path), # Log messages to a file at a specific path
-        logging.StreamHandler(sys.stdout) # Log messages to the console
+        logging.FileHandler(log_file_path),
+        logging.StreamHandler(sys.stdout) 
     ]
 )
 
-logger = logging.getLogger('kidneyDiseaseClassifierLogger') # Creates a logger object
+logger = logging.getLogger('kidneyDiseaseClassifierLogger') 
