ce3e972d4a893c7ea5d46702a61541b7ddf58454
MartinKalema <kalemaaaaaaaa@gmail.com>
2024-03-29 01:03:47 +0300

    prediction from user input

---
+++ src/kidneyDiseaseClassifier/pipeline/stage_05_prediction.py
@@ -0,0 +1,26 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import os 
+
+class PredictionPipeline:
+    def __init__(self, filename) -> None:
+        self.filename = filename
+
+
+    def predict(self):
+        model = load_model(os.path('artifacts', 'training', 'model.h5'))
+
+        imagename = self.fileename
+        test_image = image.load_img(imagename, target_size= (224,224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        result = np.argmax(model.predict(test_image), axis=1)
+        print(result)
+
+        if result[0] == 1:
+            prediction = 'Tumor'
+            return [{"image": prediction}]
+        else:
+            prediction = 'Normal'
+            return [{"image": prediction}]
